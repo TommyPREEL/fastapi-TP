16a29746ab847bcb7bf3ca6c4399f98f15b74de8
TommyPREEL <preel.tommy@gmail.com>
2024-07-23 12:48:00 +0200

    print

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/ping")
+# async def ping():
+#     return "pong"
+
+# @app.get("/pong")
+# async def ping():
+#     return "ping"
+
+
+from fastapi import FastAPI, Request, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 import boto3
 from botocore.exceptions import NoCredentialsError, ClientError
@@ -24,16 +37,30 @@ BUCKET_NAME = os.getenv('BUCKET_NAME')
 async def ping():
     return "pong"
 
-@app.put("/api/file/{filename}")
+
+# @app.put("/api/file/{filename}")
+# async def upload_file(filename: str, request: Request):
+#     try:
+#         # Lire le fichier depuis le corps de la requête
+#         file_content = await request.body()
+
+#         # Convertir le contenu en un objet BytesIO
+#         file_stream = io.BytesIO(file_content)
+        
+#         # Téléverser le fichier sur S3
+#         s3.upload_fileobj(file_stream, BUCKET_NAME, filename)
+        
+#         return JSONResponse(content={"message": "File uploaded successfully"}, status_code=200)
+#     except NoCredentialsError:
+#         raise HTTPException(status_code=500, detail="Credentials not available")
+#     except ClientError as e:
+#         raise HTTPException(status_code=500, detail=f"Failed to upload file: {e}")
+
+@app.post("/api/file/{filename}")
 async def upload_file(filename: str, file: UploadFile = File(...)):
     try:
-        # Vérifier si le fichier est bien un objet file-like
-        if not file.file:
-            raise HTTPException(status_code=400, detail="No file uploaded")
-        
         # Téléverser le fichier sur S3
         s3.upload_fileobj(file.file, BUCKET_NAME, filename)
-        
         return JSONResponse(content={"message": "File uploaded successfully"}, status_code=200)
     except NoCredentialsError:
         raise HTTPException(status_code=500, detail="Credentials not available")
@@ -42,6 +69,9 @@ async def upload_file(filename: str, file: UploadFile = File(...)):
 
 @app.get("/api/file/{filename}")
 async def download_file(filename: str):
+    print(f"AWS_ACCESS_KEY_ID: {os.getenv('AWS_ACCESS_KEY_ID')}")
+    print(f"AWS_SECRET_ACCESS_KEY: {os.getenv('AWS_SECRET_ACCESS_KEY')}")
+    print(f"BUCKET_NAME: {os.getenv('BUCKET_NAME')}")
     try:
         file_obj = s3.get_object(Bucket=BUCKET_NAME, Key=filename)
         return StreamingResponse(io.BytesIO(file_obj['Body'].read()), media_type='application/octet-stream')
