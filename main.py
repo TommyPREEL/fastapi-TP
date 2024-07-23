6fe887a3bcfbc3089ae1b4f9f00f82ae46eb369f
TommyPREEL <preel.tommy@gmail.com>
2024-07-23 14:33:58 +0200

    region_name

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app = FastAPI()
 s3 = boto3.client('s3',
                   aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
                   aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                  region=os.getenv('AWS_REGION'),
+                  region_name=os.getenv('AWS_REGION'),
                   )
 
 # Nom du bucket S3
