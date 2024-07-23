d27cc564de00aadf8ed99a1f2189636147420c6a
TommyPREEL <preel.tommy@gmail.com>
2024-07-23 14:27:36 +0200

    add region

--- main.py
+++ main.py
@@ -28,7 +28,9 @@ app = FastAPI()
 # Configuration de Boto3
 s3 = boto3.client('s3',
                   aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                  aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+                  aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                  region=os.getenv('AWS_REGION'),
+                  )
 
 # Nom du bucket S3
 AWS_S3_BUCKET_NAME = os.getenv('AWS_S3_BUCKET_NAME')
